e8f9229f2dd9ee4acd855ae22bd7a34f0cad2d48
Garrett Johnson <gjohnson49@liberty.edu>
2020-07-12 16:54:17 -0400

    Added in a bunch of stuff with text boxes, comboBoxes, and other control factors. It isn't pretty or on topic, but it does work.

--- Picture Viewer/Form1.Designer.cs
+++ Picture Viewer/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Picture_Viewer
+﻿using System.Drawing.Text;
+
+namespace Picture_Viewer
 {
     partial class Viewer
     {
@@ -28,60 +30,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.stretchCBox = new System.Windows.Forms.CheckBox();
+            this.primaryLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.imageBox = new System.Windows.Forms.PictureBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.ShowBtn = new System.Windows.Forms.Button();
             this.clearBtn = new System.Windows.Forms.Button();
             this.backgroundBtn = new System.Windows.Forms.Button();
             this.closeBtn = new System.Windows.Forms.Button();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.tableLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.wpnComboBox = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.stretchCBox = new System.Windows.Forms.CheckBox();
+            this.imageFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.backgroundColorPicker = new System.Windows.Forms.ColorDialog();
+            this.levelNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.primaryLayoutPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
             this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.levelNumericUpDown)).BeginInit();
+            this.flowLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel1
+            // primaryLayoutPanel
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 18.23144F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 81.76856F));
-            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.stretchCBox, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 1);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 79.78339F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.21661F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(916, 554);
-            this.tableLayoutPanel1.TabIndex = 0;
+            this.primaryLayoutPanel.AutoScroll = true;
+            this.primaryLayoutPanel.AutoSize = true;
+            this.primaryLayoutPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.primaryLayoutPanel.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.InsetDouble;
+            this.primaryLayoutPanel.ColumnCount = 2;
+            this.primaryLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 18.23144F));
+            this.primaryLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 81.76856F));
+            this.primaryLayoutPanel.Controls.Add(this.imageBox, 0, 0);
+            this.primaryLayoutPanel.Controls.Add(this.flowLayoutPanel1, 1, 2);
+            this.primaryLayoutPanel.Controls.Add(this.stretchCBox, 0, 1);
+            this.primaryLayoutPanel.Controls.Add(this.flowLayoutPanel2, 1, 1);
+            this.primaryLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.primaryLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.primaryLayoutPanel.Name = "primaryLayoutPanel";
+            this.primaryLayoutPanel.RowCount = 3;
+            this.primaryLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 76.18026F));
+            this.primaryLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 23.81974F));
+            this.primaryLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 105F));
+            this.primaryLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.primaryLayoutPanel.Size = new System.Drawing.Size(1173, 493);
+            this.primaryLayoutPanel.TabIndex = 0;
             // 
-            // pictureBox1
+            // imageBox
             // 
-            this.tableLayoutPanel1.SetColumnSpan(this.pictureBox1, 2);
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(910, 435);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // stretchCBox
-            // 
-            this.stretchCBox.AutoSize = true;
-            this.stretchCBox.Location = new System.Drawing.Point(3, 444);
-            this.stretchCBox.Name = "stretchCBox";
-            this.stretchCBox.Size = new System.Drawing.Size(67, 19);
-            this.stretchCBox.TabIndex = 1;
-            this.stretchCBox.Text = "Stretch";
-            this.stretchCBox.UseVisualStyleBackColor = true;
-            this.stretchCBox.CheckedChanged += new System.EventHandler(this.stretchCBox_CheckedChanged);
+            this.primaryLayoutPanel.SetColumnSpan(this.imageBox, 2);
+            this.imageBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox.Location = new System.Drawing.Point(6, 6);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(1161, 280);
+            this.imageBox.TabIndex = 0;
+            this.imageBox.TabStop = false;
             // 
             // flowLayoutPanel1
             // 
+            this.flowLayoutPanel1.AutoSize = true;
             this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.Control;
             this.flowLayoutPanel1.Controls.Add(this.ShowBtn);
             this.flowLayoutPanel1.Controls.Add(this.clearBtn);
@@ -90,17 +97,18 @@
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
             this.flowLayoutPanel1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(169, 444);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(221, 387);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(744, 107);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(946, 100);
             this.flowLayoutPanel1.TabIndex = 2;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
             // 
             // ShowBtn
             // 
             this.ShowBtn.AutoSize = true;
             this.ShowBtn.BackColor = System.Drawing.SystemColors.Control;
             this.ShowBtn.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.ShowBtn.Location = new System.Drawing.Point(642, 3);
+            this.ShowBtn.Location = new System.Drawing.Point(844, 3);
             this.ShowBtn.Name = "ShowBtn";
             this.ShowBtn.Size = new System.Drawing.Size(99, 25);
             this.ShowBtn.TabIndex = 0;
@@ -113,7 +121,7 @@
             this.clearBtn.AutoSize = true;
             this.clearBtn.BackColor = System.Drawing.SystemColors.Control;
             this.clearBtn.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.clearBtn.Location = new System.Drawing.Point(549, 3);
+            this.clearBtn.Location = new System.Drawing.Point(751, 3);
             this.clearBtn.Name = "clearBtn";
             this.clearBtn.Size = new System.Drawing.Size(87, 25);
             this.clearBtn.TabIndex = 1;
@@ -126,7 +134,7 @@
             this.backgroundBtn.AutoSize = true;
             this.backgroundBtn.BackColor = System.Drawing.SystemColors.Control;
             this.backgroundBtn.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.backgroundBtn.Location = new System.Drawing.Point(407, 3);
+            this.backgroundBtn.Location = new System.Drawing.Point(609, 3);
             this.backgroundBtn.Name = "backgroundBtn";
             this.backgroundBtn.Size = new System.Drawing.Size(136, 25);
             this.backgroundBtn.TabIndex = 2;
@@ -140,7 +148,7 @@
             this.closeBtn.BackColor = System.Drawing.SystemColors.Control;
             this.closeBtn.ForeColor = System.Drawing.SystemColors.ControlText;
             this.closeBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.closeBtn.Location = new System.Drawing.Point(326, 3);
+            this.closeBtn.Location = new System.Drawing.Point(528, 3);
             this.closeBtn.Name = "closeBtn";
             this.closeBtn.Size = new System.Drawing.Size(75, 25);
             this.closeBtn.TabIndex = 3;
@@ -148,40 +156,121 @@
             this.closeBtn.UseVisualStyleBackColor = false;
             this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
             // 
-            // openFileDialog1
+            // wpnComboBox
             // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            this.openFileDialog1.Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp";
-            this.openFileDialog1.Title = "Select a picture to open...";
+            this.wpnComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.wpnComboBox.FormattingEnabled = true;
+            this.wpnComboBox.Items.AddRange(new object[] {
+            "Axe",
+            "Bow",
+            "Spear"});
+            this.wpnComboBox.Location = new System.Drawing.Point(148, 3);
+            this.wpnComboBox.Name = "wpnComboBox";
+            this.wpnComboBox.Size = new System.Drawing.Size(121, 21);
+            this.wpnComboBox.TabIndex = 5;
+            this.wpnComboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(3, 6);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 6, 3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(139, 13);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "Weapon of Choice:";
+            // 
+            // stretchCBox
+            // 
+            this.stretchCBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.stretchCBox.Location = new System.Drawing.Point(69, 295);
+            this.stretchCBox.Name = "stretchCBox";
+            this.stretchCBox.Size = new System.Drawing.Size(80, 28);
+            this.stretchCBox.TabIndex = 1;
+            this.stretchCBox.Text = "Stretch";
+            this.stretchCBox.UseVisualStyleBackColor = true;
+            this.stretchCBox.CheckedChanged += new System.EventHandler(this.stretchCBox_CheckedChanged);
+            // 
+            // imageFileDialog
+            // 
+            this.imageFileDialog.FileName = "openFileDialog1";
+            this.imageFileDialog.Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp";
+            this.imageFileDialog.Title = "Select a picture to open...";
+            // 
+            // levelNumericUpDown
+            // 
+            this.levelNumericUpDown.Location = new System.Drawing.Point(356, 3);
+            this.levelNumericUpDown.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.levelNumericUpDown.Name = "levelNumericUpDown";
+            this.levelNumericUpDown.Size = new System.Drawing.Size(120, 20);
+            this.levelNumericUpDown.TabIndex = 1;
+            this.levelNumericUpDown.ValueChanged += new System.EventHandler(this.levelNumericUpDown_ValueChanged);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.textBox1);
+            this.flowLayoutPanel2.Controls.Add(this.wpnComboBox);
+            this.flowLayoutPanel2.Controls.Add(this.textBox2);
+            this.flowLayoutPanel2.Controls.Add(this.levelNumericUpDown);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(221, 295);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(946, 83);
+            this.flowLayoutPanel2.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(275, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(75, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "Enhancement";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // Viewer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(916, 554);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.ClientSize = new System.Drawing.Size(1173, 493);
+            this.Controls.Add(this.primaryLayoutPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Viewer";
             this.Text = "Viewer";
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.primaryLayoutPanel.ResumeLayout(false);
+            this.primaryLayoutPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
             this.flowLayoutPanel1.ResumeLayout(false);
             this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.levelNumericUpDown)).EndInit();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel primaryLayoutPanel;
+        private System.Windows.Forms.PictureBox imageBox;
         private System.Windows.Forms.CheckBox stretchCBox;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Button ShowBtn;
         private System.Windows.Forms.Button clearBtn;
         private System.Windows.Forms.Button backgroundBtn;
         private System.Windows.Forms.Button closeBtn;
-        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog imageFileDialog;
+        private System.Windows.Forms.ColorDialog backgroundColorPicker;
+        private System.Windows.Forms.ComboBox wpnComboBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown levelNumericUpDown;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- Picture Viewer/Form1.cs
+++ Picture Viewer/Form1.cs
@@ -19,29 +19,75 @@ namespace Picture_Viewer
 
         private void stretchCBox_CheckedChanged(object sender, EventArgs e)
         {
-
+            
+            if (stretchCBox.Checked)
+            {
+                imageBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                imageBox.SizeMode = PictureBoxSizeMode.Normal;
+                imageBox.SizeMode = PictureBoxSizeMode.CenterImage;
+            }
+               
+           
         }
 
         private void ShowBtn_Click(object sender, EventArgs e)
         {
-            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            //First, it calls up a dialogue box, then checks to see if it pulled a valid filename. 
+            if(imageFileDialog.ShowDialog() == DialogResult.OK)
+            /*At this point, the file is valid, so it tells the pictureBox to load the file attached to
+             the file dialog box. 
+             */
             {
-                pictureBox1.Load(openFileDialog1.FileName);
+                imageBox.Load(imageFileDialog.FileName);
+                imageBox.SizeMode = PictureBoxSizeMode.CenterImage;
             }
         }
 
         private void closeBtn_Click(object sender, EventArgs e)
         {
-
+            this.Close();
         }
 
         private void backgroundBtn_Click(object sender, EventArgs e)
         {
-            
+            if(backgroundColorPicker.ShowDialog() == DialogResult.OK) 
+            {
+                imageBox.BackColor = backgroundColorPicker.Color;
+
+            }
         }
 
         private void clearBtn_Click(object sender, EventArgs e)
         {
+            imageBox.Image = null;
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "Chosen Weapon: "+ Convert.ToString(wpnComboBox.SelectedItem);
+            Size size = TextRenderer.MeasureText(textBox1.Text, textBox1.Font);
+            textBox1.Width = size.Width;
+            textBox1.Height = size.Height;
+        }
+
+        private void levelNumericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = "Weapon Enhancement: +" + levelNumericUpDown.Value;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Size size = TextRenderer.MeasureText(textBox2.Text, textBox2.Font);
+            textBox2.Width = size.Width;
+            textBox2.Height = size.Height;
 
         }
     }
